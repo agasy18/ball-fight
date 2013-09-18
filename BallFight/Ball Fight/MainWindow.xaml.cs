08b56c51ab92357720d01aaedd02807b2c10a775
Aghasy <agasy18@gmail.com>
2013-09-18 23:55:54 +0400

    first connection created

--- BallFight/Ball Fight/MainWindow.xaml.cs
+++ BallFight/Ball Fight/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BallNetModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -24,28 +26,30 @@ namespace Ball_Fight
     public partial class MainWindow : Window
     {
         const double KeyDelta = 10;
-        Storyboard BallAnimation = new Storyboard();
         private bool First = true;
         public MainWindow()
         {
             InitializeComponent();
+            BallPosition = new Vector(Ball.Margin.Left, Ball.Margin.Top);
+            PlayerTextBox.Text = Environment.UserName;
         }
 
         private void Canvas_MouseEnter_1(object sender, MouseEventArgs e)
         {
-
+            
         }
 
         private void Canvas_MouseLeave_1(object sender, MouseEventArgs e)
         {
 
         }
-
+        
         private void Canvas_MouseMove_1(object sender, MouseEventArgs e)
         {
             var mar = Player1.Margin;
             mar.Left = Math.Min(Math.Max(0, e.GetPosition(GameBoard).X - Player1.Width / 2), GameBoard.Width - Player1.Width);
             Player1.Margin = mar;
+            Ball.Margin = BallMagine;
         }
 
         private void CurWindow_Activated(object sender, EventArgs e)
@@ -84,66 +88,151 @@ namespace Ball_Fight
                 }
             }
         }
-
+        int fpsCounter = 0;
         private void GameBoard_LayoutUpdated(object sender, EventArgs e)
         {
-
+            fpsCounter++;
+            Ball.Margin=BallMagine;           
         }
-
+        
         private void CurWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-
-
+            
         }
-        Timer timer = new Timer(10);
-        private void CurWindow_Loaded(object sender, RoutedEventArgs e)
+
+        bool diasbleTimer = false;
+        
+        private async void CurWindow_Loaded(object sender, RoutedEventArgs e)
         {
             if (First)
             {
                 First = true;
-
-                timer.Elapsed += (s, v) =>
+                LoadUpdateSystem();
+                LoadSpeedSystem();
+                LoadFpsSystem();
+                Start();        
+                for (; ;)
                 {
-                    try
+                    if (diasbleTimer == false)
                     {
-                        this.Dispatcher.Invoke(new Action(() => { tick(); }));
+                        for (int i = 0; i < gameSpeed; i++)                        
+                        tick();
                     }
-                    catch
-                    { }
-                };
-                timer.Enabled = false;
-                Start();
+                    await Task.Delay(1);
+                }
+               
             }
         }
+
+        private void LoadFpsSystem()
+        {
+            Timer fpsTimer;
+            fpsTimer = new Timer(1000);
+            fpsTimer.Elapsed += (s, q) =>
+            {
+                int fps = fpsCounter;
+                fpsCounter = 0;
+                fpsLabel.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    fpsLabel.Content ="FPS: " + fps;
+                }));
+            };
+            fpsTimer.Start();
+        }
+        private void LoadUpdateSystem()
+        {
+            Timer fpsTimer;
+            fpsTimer = new Timer(1000/60);
+            fpsTimer.Elapsed += (s, q) =>
+            {
+                fpsLabel.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    GameBoard_LayoutUpdated(null,null);
+                }));
+            };
+            
+            fpsTimer.Start();
+        }
+        private void LoadSpeedSystem()
+        {
+            Timer fpsTimer;
+            fpsTimer = new Timer(1000 * 2);
+            GameSpeedLabel.Content = gameSpeed;
+            fpsTimer.Elapsed += (s, q) =>
+            {
+                fpsLabel.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    if (gameSpeed > 10) 
+                        return;
+                    gameSpeed++;
+                    
+                    GameSpeedLabel.Content = gameSpeed;
+                }));
+            };
+
+            fpsTimer.Start();
+        }
+
+        int gameSpeed = 1;
         double ballAngel;
-        const double ballV = 2;
+        const double ballV = 1;
+
+        Vector BallPosition;
+        Thickness BallMagine
+        {
+            get
+            {
+                return new Thickness(BallPosition.X, BallPosition.Y, BallPosition.X + Ball.Width, BallPosition.Y + Ball.Height);
+            }
+        }
+        Vector BallVelocity
+        {
+            get
+            {
+                var dx = ballV * Math.Sin(ballAngel);
+                var dy = ballV * Math.Cos(ballAngel); 
+                return new Vector(dx, dy);
+            }
+        }
         private void tick()
         {
-            var mar = Ball.Margin;
-            mar.Left += ballV * Math.Sin(ballAngel);
-            mar.Top += ballV * Math.Cos(ballAngel);
-            if (mar.Left <= 0 || mar.Left>= GameBoard.Width-Ball.Width )
+            BallPosition += BallVelocity;
+            var mar = BallMagine;         
+            if (mar.Left <= 0)
             {
-                ballAngel = - ballAngel;
-                tick();
-                return;
+                ballAngel = -ballAngel;
             }
-                        if (Kpav(mar,Player1) || Kpav (mar,Player2))
+            else if(mar.Right >= GameBoard.Width )
             {
-                ballAngel = Math.PI- ballAngel;
-                tick();
-                return;
+                ballAngel = -ballAngel;
             }
-           
-            Ball.Margin = mar;
+
+            else if (mar.Top <= Player2.Margin.Top + Player2.Height && mar.Right - 2 > Player2.Margin.Left && mar.Left + 2 < Player2.Margin.Left + Player2.Width)
+            {
+                ballAngel = Math.PI - ballAngel;
+            }
+            else if (mar.Bottom >= Player1.Margin.Top && mar.Right-2>Player1.Margin.Left && mar.Left+2<Player1.Margin.Left+Player1.Width)
+            {
+                ballAngel = Math.PI - ballAngel;
+            }
+            else if (mar.Top <= 0)
+            {
+                GameOver(Player2Label.Content as string);
+                ballAngel = Math.PI - ballAngel;
+            }
+            else if (mar.Bottom >= GameBoard.Height )
+            {
+                GameOver(PlayerTextBox.Text);
+                ballAngel = Math.PI - ballAngel;
+            }
         }
 
-        private bool Kpav(Thickness mar, Rectangle Player)
+        private void GameOver(string p)
         {
-            return mar.Top <= 0 || mar.Top >= GameBoard.Height - Ball.Height;
-           
+            p.Log("Game Over");
         }
 
+     
         
 
 
@@ -161,11 +250,42 @@ namespace Ball_Fight
         {
             Start();
         }
-
+        private ChannelFactory<IBallService> remoteFactory;
         private void Start()
         {
-            timer.Enabled = true;
             ballAngel =- Math.PI / 3;
+            try
+            {
+                lblStatus.Content = "Connecting...";
+                if (!String.IsNullOrEmpty(PlayerTextBox.Text))
+                {
+                    remoteFactory = new ChannelFactory<IBallService>("BallConfig");
+                    remoteProxy = remoteFactory.CreateChannel();
+                    clientUser = remoteProxy.ClientConnect(PlayerTextBox.Text,KeyTextBox.Text);
+
+                    if (clientUser != null)
+                    {
+                        ConfigConnectedState();
+                        lblStatus.Content = "Connected as: " + clientUser.UserName;
+                    }
+                }
+                else
+                    lblStatus.Content = "Disconnected";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has ocurred\nClient cannot connect\nMessage:" + ex.Message,
+                    "FATAL ERROR", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
         }
+
+        private void ConfigConnectedState()
+        {
+
+        }
+
+        public IBallService remoteProxy { get; set; }
+
+        public BallUser clientUser { get; set; }
     }
 }
